3f2a86f027bc4666554c9a0f9eff2074349b631c
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-03-02 00:53:49 -0500

    Create the 512 element subset of the Y vector

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod group_utils;
 
-use rand::{distributions::Uniform, Rng}; // 0.6.5
+use rand::{distributions::Uniform, Rng, seq::SliceRandom}; // 0.6.5
 use group_utils::BaseGroup;
 fn main() {
     let mut rng = rand::thread_rng();
+    //TODO: We should use a 256 bit prime number for the upper bound
     let range = Uniform::new(1, 5000);
     let secret_set: Vec<u64> = (0..1024).map(|_| rng.sample(&range)).collect();
     let G = BaseGroup::new();
-    let Y: Vec<(primitive_types::U512,primitive_types::U512)> = secret_set.iter().map(|n| G.exponentiate(*n)).collect();
-
+    let Y_1024: Vec<(primitive_types::U512,primitive_types::U512)> = secret_set.iter().map(|n| G.exponentiate(*n)).collect();
+    println!("{:?} ", Y_1024);
+    let Y_512 = Y_1024.choose_multiple( &mut rand::thread_rng(), 512);
+    
 }
