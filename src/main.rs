514c61a7f0630845d20ecdc4aa69e78d48bfa123
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-03-03 09:35:17 -0500

    Split up logic for subset and sha address gen

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
 mod group_utils;
+mod sha_generator;
+mod subset_generator;
 
-use rand::{distributions::Uniform, Rng, seq::SliceRandom}; // 0.6.5
-use group_utils::BaseGroup;
 fn main() {
-    let mut rng = rand::thread_rng();
-    //TODO: We should use a 256 bit prime number for the upper bound
-    let range = Uniform::new(1, 5000);
-    let secret_set: Vec<u64> = (0..1024).map(|_| rng.sample(&range)).collect();
-    let G = BaseGroup::new();
-    let Y_1024: Vec<(primitive_types::U512,primitive_types::U512)> = secret_set.iter().map(|n| G.exponentiate(*n)).collect();
-    println!("{:?} ", Y_1024);
-    let Y_512 = Y_1024.choose_multiple( &mut rand::thread_rng(), 512);
-    
+    sha_generator::sha_generate_address();
 }
---
+++ src/sha_generator.rs
@@ -0,0 +1,18 @@
+use crate::group_utils;
+use primitive_types::U256;
+
+fn SHA_PRIME() -> U256 {
+    let result: i32 = 2 ^ 256 - 2 ^ 32 - 2 ^ 9 - 2 ^ 8 - 2 ^ 7 - 2 ^ 6 - 2 ^ 4 - 1;
+    let conv = U256::from_str_radix(result.to_string().as_str(), 10);
+    let SHA_PRIME = match conv {
+        Ok(conv) => conv,
+        Err(e) => panic!("Could not parse SHA_PRIME: {}", e),
+    };
+    return SHA_PRIME;
+}
+
+pub fn sha_generate_address() {
+    let G = group_utils::BaseGroup::new();
+    let summation_result = G.exponentiate_from_256_bit_number(SHA_PRIME());
+    println!("{:?} ", summation_result);
+}
