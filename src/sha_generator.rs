8174eb0d0f245dbc138d4c0431735ab60ac6a511
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-03-03 10:17:24 -0500

    SHA2 one part of the coordinate point

--- src/sha_generator.rs
+++ src/sha_generator.rs
@@ -1,5 +1,6 @@
 use crate::group_utils;
 use primitive_types::U256;
+use sha2::{Digest, Sha256};
 
 fn SHA_PRIME() -> U256 {
     let result: i32 = 2 ^ 256 - 2 ^ 32 - 2 ^ 9 - 2 ^ 8 - 2 ^ 7 - 2 ^ 6 - 2 ^ 4 - 1;
@@ -15,4 +16,8 @@ pub fn sha_generate_address() {
     let G = group_utils::BaseGroup::new();
     let summation_result = G.exponentiate_from_256_bit_number(SHA_PRIME());
     println!("{:?} ", summation_result);
+    let mut hasher = Sha256::new();
+    hasher.update(summation_result.0.to_string().as_bytes());
+    let result = hasher.finalize();
+    println!("{:?} ", result);
 }
